310de8bc3b87b11bbb1b2310579a0265b33a6c80
mysoyul <vega2k_@naver.com>
2022-04-07 16:47:39 +0900

    userservice

--- src/main/java/com/basic/myspringboot/service/AccountService.java
+++ src/main/java/com/basic/myspringboot/service/AccountService.java
@@ -1,9 +1,14 @@
 package com.basic.myspringboot.service;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class AccountService implements UserDetailsService{
+public class AccountService implements UserDetailsService {
 	private final AccountRepository accountRepository;
 	private final MybootProperties property;
 
@@ -35,10 +40,19 @@ public class AccountService implements UserDetailsService{
 			System.out.println(newAccount);
 		}
 	}
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Account> byUsername = accountRepository.findByUsername(username);
+		Account account = byUsername.orElseThrow(() -> new UsernameNotFoundException(username));
+		return new User(account.getUsername(), 
+		account.getPassword(), authorities());
+
 	}
+
+	// User 객체의 세번째 인자 USER라는 ROLE을 가진 사용자이다 라고 설정하는 부분
+	private Collection<? extends GrantedAuthority> authorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
 }
