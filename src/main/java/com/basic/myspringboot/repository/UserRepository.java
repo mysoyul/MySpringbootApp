a00666ff0ddb83f1884a41494e5658598c117e03
mysoyul <vega2k_@naver.com>
2022-04-07 10:23:40 +0900

    user repository

--- src/main/java/com/basic/myspringboot/repository/UserRepository.java
+++ src/main/java/com/basic/myspringboot/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.basic.myspringboot.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.basic.myspringboot.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	
+	Optional<User> findByEmail(String email);
 }
