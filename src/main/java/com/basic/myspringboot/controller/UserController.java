8eaa2c4a365b81b5b415bf1ea428c777d7b6dd2c
mysoyul <vega2k_@naver.com>
2022-04-07 14:33:55 +0900

    usercontroller

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -4,13 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.basic.myspringboot.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+	private final UserRepository userRepository;
+
+//	public UserController(UserRepository userRepository) {
+//		this.userRepository = userRepository;
+//	}
 
 	@GetMapping("/thymeleaf")
 	public String leaf(Model model) {
 		model.addAttribute("name", "스프링부트");
 		return "leaf";
 	}
-	
+
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		return "index";
+	}
+
 }
