c03de37af3e9177fcd414a62b012efbad39c2958
mysoyul <vega2k_@naver.com>
2022-04-07 10:49:41 +0900

    user controller

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,14 +1,22 @@
 package com.basic.myspringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.basic.myspringboot.entity.User;
 import com.basic.myspringboot.repository.UserRepository;
 
 @RestController
+@RequestMapping("/users")
 public class UserRestController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	
+	@PostMapping
+	public User saveUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
 }
