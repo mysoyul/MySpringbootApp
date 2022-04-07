a91e2304fe649c9cf9a172eec7542a54fe3d24b7
mysoyul <vega2k_@naver.com>
2022-04-07 15:05:53 +0900

    add user html

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -1,21 +1,25 @@
 package com.basic.myspringboot.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.basic.myspringboot.entity.User;
 import com.basic.myspringboot.repository.UserRepository;
 
-import lombok.RequiredArgsConstructor;
-
 @Controller
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class UserController {
 	private final UserRepository userRepository;
 
-//	public UserController(UserRepository userRepository) {
-//		this.userRepository = userRepository;
-//	}
+	// constructor injection
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	@GetMapping("/thymeleaf")
 	public String leaf(Model model) {
@@ -25,6 +29,21 @@ public class UserController {
 
 	@GetMapping("/index")
 	public String index(Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		return "index";
+	}
+
+	@GetMapping("/signup")
+	public String showSignUpForm(User user) {
+		return "add-user";
+	}
+
+	@PostMapping("/adduser")
+	public String addUser(@Valid User user, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "add-user";
+		}
+		userRepository.save(user);
 		model.addAttribute("users", userRepository.findAll());
 		return "index";
 	}
